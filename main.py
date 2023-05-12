e4577d976128c04e80b67186a8a6e1845a1e1c0a
Shintaro Yamaki <97502358+FosterYamaki@users.noreply.github.com>
2023-05-12 11:03:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ import math
 import glob
 from utils.m10_processing import M10_Processing, m10_batch
 
-m10_batch(folder_path="./M10", output_dir="./XLSX")
+if __name__ == '__main__':
+    m10_batch(folder_path="./M10", output_dir="./XLSX")
