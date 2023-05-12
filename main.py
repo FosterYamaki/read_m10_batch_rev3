5ff243edd705787e1c1fd0d5c60e560a4f1c3b61
Shintaro Yamaki <97502358+FosterYamaki@users.noreply.github.com>
2023-05-12 10:49:16 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import glob
+from utils.m10_processing import M10_Processing, m10_batch
+
+m10_batch(folder_path="./M10", output_dir="./XLSX")
